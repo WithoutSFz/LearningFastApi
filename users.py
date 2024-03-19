23126cbb3615f7ed1f201b8efa9689cc91680624
MarcoFernandez <marcoeze14@gmail.com>
2024-03-19 00:10:53 -0300

    Primer commit

---
+++ users.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app= FastAPI()
+
+class User(BaseModel):
+    id:int
+    name:str
+    surname:str
+    url:str
+    age:int
+
+users_list =[User(id=1,name= "Marco",surname= "Fernandez",url="http:\\marco.com",age=24),
+             User(id=2,name= "Marco2",surname= "Fernandez",url="http:\\marco.com",age=24),
+             User(id=3,name= "Marco3",surname= "Fernandez",url="http:\\marco.com",age=24)]
+@app.get("/user")
+async def usersclass():
+    return users_list    
+@app.get("/user/{id}")
+async def user(id:int):
+    users=filter(lambda user:user.id==id,users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error":"No se ha encontrado el usuario"}
