89da50c59f7dbcb8f79c1877c39b2f680ba697d6
MarcoFernandez <marcoeze14@gmail.com>
2024-03-20 21:55:14 -0300

    OAuth2 Finalizada

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -15,7 +15,7 @@ class User(BaseModel):
 class UserDB(User):
     password:str
 
-user_db={
+users_db={
          "WithoutS":
            {
              "user_name":"WithoutS",
@@ -34,27 +34,30 @@ user_db={
            }           
         }
 def user_search(username:str):
-    if username in user_db:
-        return UserDB(user_db[username])
+    if username in users_db:
+        return User(**users_db[username])
     
 async def current_user(token:str=Depends(oauth2)):
     user = user_search(token)
     if not user :
         raise HTTPException( status_code=401, detail="Credencial de autenticacion invalida")
+    if user.disabled:
+        raise HTTPException( 
+             status_code=400, detail="El usuario no esta activo")
     return user
     
 @app.post("/login")
 async def login(form:OAuth2PasswordRequestForm= Depends()):
-    user_db = user_db.get(form.username)
+    user_db = users_db.get(form.username)
     if not user_db:
         raise HTTPException( 
-             status_code=400, detail="El usuario no es correcta")
-    user = user_search(form.password)
+             status_code=400, detail="El usuario no es correcto")   
+    user = user_search(form.username)
     if not form.password == user.password:
         raise HTTPException( status_code=400, detail="La contraseña no es correcta")
 
-    return {"acces_token":user.username,"token_type":"bearer"}    
+    return {"acces_token":user.user_name,"token_type":"bearer"}    
 
-@app.get("/user/me")
-async def me(user:User= Depends(current_user())):
+@app.get("/users/me")
+async def me(user:User= Depends(current_user)):
     return user
